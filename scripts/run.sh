9081a1d7b8d407a94670b00a2356efaeab260978
Sébastien Ferry <sferry@smartadserver.com>
2024-11-26 22:36:23 +0100

    Add script to run Docker container with configuration and environment variables

---
+++ scripts/run.sh
@@ -0,0 +1,17 @@
+# Compute the parent directory
+current_dir=$(pwd)
+parent_dir=$(dirname "$current_dir")
+
+# Run the docker container
+# Expose the port 3000 for prometheus and healthcheck
+# Mount the configuration file in the container
+# Load the environment variables from the .env file
+# - CONFIG_FILE_PATH: /app/conf.yaml
+# - SOURCE: mongodb://<mongo_uri>:27017/?replicaSet=<replica_set>
+# - TARGET: mongodb://<mongo_uri>:27017/?replicaSet=<replica_set>
+# Run the docker container
+docker run -it --rm \
+-p 3000:3000 \
+--mount type=bind,source=$parent_dir/conf/config.dev.yaml,target=/app/conf.yaml,readonly \
+--env-file $parent_dir/conf/.env \
+sebastienferry/mongo-repl:v0.0.8
